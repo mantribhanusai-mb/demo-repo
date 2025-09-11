aec0c4e3861135981742f0e7fca7b7c9cb0b8e27
mantribhanusai-mb <mantribhanusai@gmail.com>
2025-09-11 22:55:39 +0530

    updated

---
+++ 02_variables/demo.py
@@ -0,0 +1,20 @@
+#working with variables
+
+#assigning data
+
+student_name = "Bhanusai"
+student_age = 24
+student_grade = 10 
+
+#retrieve data
+print(student_name)
+print(student_age)
+print(student_grade)
+
+#getting identity / memory
+print(id(student_name))
+print(id(student_age))
+
+#getting type of data
+print(type(student_name))
+print(type(student_age))
