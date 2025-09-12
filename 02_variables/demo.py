788d35c776053c7b049bd9a71fed90a64bfc1b15
mantribhanusai-mb <mantribhanusai@gmail.com>
2025-09-13 00:27:53 +0530

    updated

--- 02_variables/demo.py
+++ 02_variables/demo.py
@@ -29,3 +29,29 @@ print("after addition:", x )
 
 x = "salaar"  #type can also change
 print("after reassignment:", x)
+
+#python memory model
+a = 10
+print(id(a))
+b = 20
+print(id(b))
+c = 10
+print(id(c))
+
+# a,b,c int which is simple data type (one only value)
+
+#python has complex data types (more than one value)
+
+list_num_a = [10,20,30,40,50]  #list is created using [value1,value2,....value-n]
+print(type(list_num_a))
+print(id(list_num_a))
+
+list_num_b = [10,20,30,40,50]  
+print(type(list_num_b))
+print(id(list_num_b))
+
+#to access data inside list we use index -> index starts from o 1 2....
+
+print(list_num_a[0])
+print(list_num_b[2])
+ 
